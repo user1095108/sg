168f5a62919c9c20b3156b1c28e6f6a4bf2d7d92
Janez Zemva <janezz55@gmail.com>
2021-12-01 22:50:28 +0100

    some fixes

--- common.hpp
+++ common.hpp
@@ -118,7 +118,7 @@ auto root() const noexcept { return root_; }
 
 //
 void clear() noexcept(noexcept(delete root_)) { delete root_; root_ = {}; }
-bool empty() const noexcept { return root_; }
+bool empty() const noexcept { return !root_; }
 auto max_size() const noexcept { return ~size_type{} / 3; }
 void swap(this_class& o) noexcept { std::swap(root_, o.root_); }
 
