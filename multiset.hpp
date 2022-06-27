ad08630efcc77dfba2319b14967140d30c409153
Janez Žemva <janezz55@gmail.com>
2022-06-27 10:52:33 +0200

    some fixes

--- multiset.hpp
+++ multiset.hpp
@@ -392,15 +392,15 @@ public:
 };
 
 //////////////////////////////////////////////////////////////////////////////
-template <typename K, typename V, class C>
-inline auto erase(multiset<K, V, C>& c, auto const& k)
+template <typename K, class C>
+inline auto erase(multiset<K, C>& c, auto const& k)
   noexcept(noexcept(c.erase(k)))
 {
   return c.erase(k);
 }
 
-template <typename K, typename V, class C>
-inline auto erase_if(multiset<K, V, C>& c, auto pred)
+template <typename K, class C>
+inline auto erase_if(multiset<K, C>& c, auto pred)
   noexcept(noexcept(c.erase(c.begin())))
 {
   typename std::remove_reference_t<decltype(c)>::size_type r{};
@@ -414,8 +414,8 @@ inline auto erase_if(multiset<K, V, C>& c, auto pred)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-template <typename K, typename V, class C>
-inline void swap(multiset<K, V, C>& l, decltype(l) r) noexcept { l.swap(r); }
+template <typename K, class C>
+inline void swap(multiset<K, C>& l, decltype(l) r) noexcept { l.swap(r); }
 
 }
 
