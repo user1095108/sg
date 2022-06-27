d1f1d90c568e4e1505a5896049bcf9e1644bb0b0
Janez Žemva <janezz55@gmail.com>
2022-06-27 10:16:46 +0200

    some fixes

--- common.hpp
+++ common.hpp
@@ -205,13 +205,13 @@ const_iterator upper_bound(auto const& k) const noexcept
 
 //
 friend auto erase(this_class& c, auto const& k)
-  noexcept(noexcept(delete root_))
+  noexcept(noexcept(node::erase(root_, k)))
 {
   return c.erase(k);
 }
 
 friend auto erase_if(this_class& c, auto pred)
-  noexcept(noexcept(delete root_))
+  noexcept(noexcept(node::erase(root_, c.begin())))
 {
   size_type r{};
 
