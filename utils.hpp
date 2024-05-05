0f6f715f610f10b44b4aaeb069d7a1b600eaf9b0
Janez Žemva <janezz55@gmail.com>
2024-05-05 08:55:35 +0200

    some fixes

--- utils.hpp
+++ utils.hpp
@@ -344,7 +344,7 @@ inline auto rebalance(auto const n, size_type const sz) noexcept
   return S::f(a, s.b_ - 1);
 }
 
-auto emplace(auto& n, auto const& k, auto const& create_node)
+inline auto emplace(auto& n, auto const& k, auto const& create_node)
   noexcept(noexcept(create_node()))
 {
   using node_t = std::remove_pointer_t<std::remove_reference_t<decltype(n)>>;
@@ -361,8 +361,7 @@ auto emplace(auto& n, auto const& k, auto const& create_node)
     {
       if (!n)
       {
-        s_ = true;
-        n = q_ = create_node_();
+        s_ = true; n = q_ = create_node_();
 
         return 1;
       }
